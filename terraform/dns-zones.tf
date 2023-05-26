98375d7e58824ade507f3547e3043d8ceb2b0ea8
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-05-26 20:09:10 +0100

    Fixes

--- terraform/dns-zones.tf
+++ terraform/dns-zones.tf
@@ -38,8 +38,8 @@ resource "azurerm_private_dns_zone" "dns" {
 resource "azurerm_private_dns_zone_virtual_network_link" "dns" {
   for_each = { for each in local.location_private_dns_zones : each.key => each }
 
-  name                  = format("link-%s-%s", each.key, lower(random_string.location[each.location].result))
-  resource_group_name   = azurerm_resource_group.vnet[var.primary_location].name
+  name                  = format("link-%s-%s", each.key, lower(random_string.location[each.value.location].result))
+  resource_group_name   = azurerm_resource_group.vnet[each.value.location].name
   private_dns_zone_name = azurerm_private_dns_zone.dns[each.key].name
-  virtual_network_id    = azurerm_virtual_network.apps[each.value].id
+  virtual_network_id    = azurerm_virtual_network.apps[each.value.location].id
 }
