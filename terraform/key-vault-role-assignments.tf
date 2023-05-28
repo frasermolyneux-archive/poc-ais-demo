aeeed93b18e37645ed59dc871f0846c8ff9fcba5
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-05-28 16:39:01 +0100

    Adding key vault role assignment

--- terraform/key-vault-role-assignments.tf
+++ terraform/key-vault-role-assignments.tf
@@ -14,6 +14,14 @@ resource "azurerm_role_assignment" "kv_demo" {
   principal_id         = "3270dd31-29ac-486d-8a16-e9179660c8d7"
 }
 
+resource "azurerm_role_assignment" "kv_demo_io" {
+  for_each = toset(var.locations)
+
+  scope                = azurerm_key_vault.kv[each.value].id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = "ad7a863d-b087-4df5-833e-2900d58b14d2"
+}
+
 resource "azurerm_role_assignment" "app" {
   for_each = toset(var.locations)
 
