5697cff34f3d00198d2713951cb901f828ba783a
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-05-28 08:38:37 +0100

    Adding function app

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,19 @@
-output "web_app_names" {
-  value = values(azurerm_linux_web_app.app)[*].name
+locals {
+  web_apps = [for web_app in azurerm_linux_web_app.app : {
+    name                = web_app.name
+    resource_group_name = web_app.resource_group_name
+  }]
+
+  func_apps = [for func_app in azurerm_linux_function_app.func : {
+    name                = func_app.name
+    resource_group_name = func_app.resource_group_name
+  }]
+}
+
+output "web_apps" {
+  value = local.web_apps
+}
+
+output "func_apps" {
+  value = local.func_apps
 }
