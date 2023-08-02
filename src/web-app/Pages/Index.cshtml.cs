6fe8d48831625e34b8c59a7326b0cabaee6fb42c
Fraser Molyneux <fmolyneux@microsoft.com>
2023-08-02 13:11:11 +0100

    Pass correlation id through to service bus

--- src/web-app/Pages/Index.cshtml.cs
+++ src/web-app/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Azure.Messaging.ServiceBus;
 
+
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace web_app.Pages
@@ -20,11 +21,32 @@ namespace web_app.Pages
 
         public async Task OnGet()
         {
+            // If there is a Request-Id received from the upstream service, set the telemetry context accordingly.
+            var requestId = string.Empty;
+            if (HttpContext.Request.Headers.ContainsKey("Request-Id"))
+            {
+                requestId = HttpContext.Request.Headers["Request-Id"];
+            }
+
             await using (var client = new ServiceBusClient(configuration["servicebus_connection_string"]))
             {
                 var sender = client.CreateSender("from_website");
-                await sender.SendMessageAsync(new ServiceBusMessage($"Hello from the web application at {Region}!"));
+                await sender.SendMessageAsync(new ServiceBusMessage($"Hello from the web application at {Region}!")
+                {
+                    CorrelationId = requestId
+                });
             }
+        }
+
+        public static string GetOperationId(string id)
+        {
+            // Returns the root ID from the '|' to the first '.' if any.
+            int rootEnd = id.IndexOf('.');
+            if (rootEnd < 0)
+                rootEnd = id.Length;
+
+            int rootStart = id[0] == '|' ? 1 : 0;
+            return id.Substring(rootStart, rootEnd - rootStart);
         }
     }
 }
