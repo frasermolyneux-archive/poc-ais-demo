bedafb582fd05a10a869f3da3f64053781f98fba
Fraser Molyneux <fmolyneux@microsoft.com>
2023-08-03 15:18:20 +0100

    Update

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,6 +4,14 @@ locals {
     resource_group_name = web_app.resource_group_name
   }]
 
+  func_apps = [for role in var.func_app_roles : {
+    role = role
+    instances = [for func_app in azurerm_linux_function_app.func : {
+      name                = func_app.name
+      resource_group_name = func_app.resource_group_name
+    } if func_app.name == role]
+  }]
+
   logic_apps = [for logic_app in azurerm_logic_app_standard.logic : {
     name                = logic_app.name
     resource_group_name = logic_app.resource_group_name
@@ -15,12 +23,7 @@ output "web_apps" {
 }
 
 output "func_apps" {
-  for_each = { for role in var.func_app_roles : each.key => each }
-
-  value = [for each in local.func_apps : {
-    name                = azurerm_linux_function_app.func[each].name
-    resource_group_name = azurerm_linux_function_app.func[each].resource_group_name
-  } if each.value.role == each]
+  value = local.func_apps
 }
 
 output "logic_apps" {
