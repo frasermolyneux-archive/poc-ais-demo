a2e66717dabc91f388c55bad9e073e20f5c7fe80
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-08-03 21:47:58 +0100

    Trying a map instead

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,13 +4,21 @@ locals {
     resource_group_name = web_app.resource_group_name
   }]
 
-  func_apps = [for role in var.func_app_roles : {
-    role = role
-    instances = [for func_app in local.func_apps_instances : {
-      name                = azurerm_linux_function_app.func[func_app.key].name
-      resource_group_name = azurerm_linux_function_app.func[func_app.key].resource_group_name
-    } if func_app.role == role]
-  }]
+  func_apps = flatten([
+    for role in var.func_app_roles : [
+      for func_app in local.func_apps_instances : {
+        key = role
+        instances = [for func_app in local.func_apps_instances : {
+          name                = azurerm_linux_function_app.func[func_app.key].name
+          resource_group_name = azurerm_linux_function_app.func[func_app.key].resource_group_name
+        } if func_app.role == role]
+      }
+    ]
+  ])
+
+  func_app_map = { for item in local.func_apps :
+    keys(item)[0] => values(item)[0]
+  }
 
   logic_apps = [for logic_app in azurerm_logic_app_standard.logic : {
     name                = logic_app.name
@@ -23,7 +31,7 @@ output "web_apps" {
 }
 
 output "func_apps" {
-  value = local.func_apps
+  value = local.func_app_map
 }
 
 output "logic_apps" {
