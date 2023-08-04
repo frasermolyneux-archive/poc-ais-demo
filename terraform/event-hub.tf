08ad7e45d6e380f4a2cb02c5e91a3a08494d28bf
Fraser Molyneux <fmolyneux@microsoft.com>
2023-08-04 08:58:46 +0100

    Adding event hub cluster

---
+++ terraform/event-hub.tf
@@ -0,0 +1,46 @@
+resource "azurerm_resource_group" "eh" {
+  for_each = toset(var.locations)
+
+  name     = format("rg-eh-%s-%s-%s", random_id.environment_id.hex, var.environment, each.value)
+  location = each.value
+
+  tags = var.tags
+}
+
+resource "azurerm_eventhub_cluster" "eh" {
+  for_each = toset(var.locations)
+
+  name = format("eh-%s-%s-%s", random_id.environment_id.hex, var.environment, each.value)
+
+  resource_group_name = azurerm_resource_group.eh[each.value].name
+  location            = azurerm_resource_group.eh[each.value].location
+
+  tags = var.tags
+
+  sku_name = "Dedicated_1"
+}
+
+resource "azurerm_private_endpoint" "eh" {
+  for_each = toset(var.locations)
+
+  name = format("pe-%s-servicebus", azurerm_eventhub_cluster.eh[each.value].name)
+
+  resource_group_name = azurerm_resource_group.eh[each.value].name
+  location            = azurerm_resource_group.eh[each.value].location
+
+  subnet_id = azurerm_subnet.endpoints[each.value].id
+
+  private_dns_zone_group {
+    name = "default"
+    private_dns_zone_ids = [
+      azurerm_private_dns_zone.dns["servicebus"].id,
+    ]
+  }
+
+  private_service_connection {
+    name                           = format("pe-%s-namespace", azurerm_eventhub_cluster.eh[each.value].name)
+    private_connection_resource_id = azurerm_eventhub_cluster.eh[each.value].id
+    subresource_names              = ["namespace"]
+    is_manual_connection           = false
+  }
+}
