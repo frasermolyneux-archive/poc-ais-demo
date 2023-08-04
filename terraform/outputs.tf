ba3a93a8ad7d5bd72d5b0b976d6b116bae9a2552
Fraser Molyneux <fmolyneux@microsoft.com>
2023-08-04 08:44:34 +0100

    Reworking

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,23 +4,39 @@ locals {
     resource_group_name = web_app.resource_group_name
   }]
 
+  //func_apps = [
+  //  for role in var.func_app_roles : {
+  //    key = role
+  //    instances = [for func_app in local.func_apps_instances : {
+  //      name                = azurerm_linux_function_app.func[func_app.key].name
+  //      resource_group_name = azurerm_linux_function_app.func[func_app.key].resource_group_name
+  //    } if func_app.role == role]
+  //  }
+  //]
+
   func_apps = [
-    for role in var.func_app_roles : {
-      key = role
-      instances = [for func_app in local.func_apps_instances : {
-        name                = azurerm_linux_function_app.func[func_app.key].name
-        resource_group_name = azurerm_linux_function_app.func[func_app.key].resource_group_name
-      } if func_app.role == role]
+    for func_app in local.func_apps_instances : {
+      role                = func_app.role
+      name                = azurerm_linux_function_app.func[func_app.key].name
+      resource_group_name = azurerm_linux_function_app.func[func_app.key].resource_group_name
     }
   ]
 
+  //logic_apps = [
+  //  for role in var.logic_app_roles : {
+  //    key = role
+  //    instances = [for logic_app in local.logic_apps_instances : {
+  //      name                = azurerm_logic_app_standard.logic[logic_app.key].name
+  //      resource_group_name = azurerm_logic_app_standard.logic[logic_app.key].resource_group_name
+  //    } if logic_app.role == role]
+  //  }
+  //]
+
   logic_apps = [
-    for role in var.logic_app_roles : {
-      key = role
-      instances = [for logic_app in local.logic_apps_instances : {
-        name                = azurerm_logic_app_standard.logic[logic_app.key].name
-        resource_group_name = azurerm_logic_app_standard.logic[logic_app.key].resource_group_name
-      } if logic_app.role == role]
+    for logic_app in local.logic_apps_instances : {
+      role                = logic_app.role
+      name                = azurerm_logic_app_standard.logic[logic_app.key].name
+      resource_group_name = azurerm_logic_app_standard.logic[logic_app.key].resource_group_name
     }
   ]
 }
@@ -30,9 +46,11 @@ output "web_apps" {
 }
 
 output "func_apps" {
-  value = { for item in local.func_apps : "${item.key}" => item }
+  //value = { for item in local.func_apps : "${item.key}" => item }
+  value = local.func_apps
 }
 
 output "logic_apps" {
-  value = { for item in local.logic_apps : "${item.key}" => item }
+  //value = { for item in local.logic_apps : "${item.key}" => item }
+  value = local.logic_apps
 }
