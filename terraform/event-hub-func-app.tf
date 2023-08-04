757d8806e432a574ae7c4a7bc23671e259c1522f
Fraser Molyneux <fmolyneux@microsoft.com>
2023-08-04 13:40:30 +0100

    Fix typo

--- terraform/event-hub-func-app.tf
+++ terraform/event-hub-func-app.tf
@@ -2,7 +2,7 @@ resource "azurerm_eventhub_authorization_rule" "func" {
   for_each = { for each in local.func_apps_instances : each.key => each }
 
   name           = azurerm_linux_function_app.func[each.key].name
-  namespace_name = azurerm_eventhub_namespace.ech[each.value.location].name
+  namespace_name = azurerm_eventhub_namespace.eh[each.value.location].name
 
   eventhub_name       = azurerm_eventhub.eh_business_notifications[each.value.location].name
   resource_group_name = azurerm_resource_group.eh[each.value.location].name
