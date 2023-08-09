b5f7b2f468de91812e46f6af9c28be2103f5474c
Fraser Molyneux <fmolyneux@microsoft.com>
2023-08-09 11:01:16 +0100

    Update event hub connection auth

--- terraform/event-hub-func-app.tf
+++ terraform/event-hub-func-app.tf
@@ -1,22 +1,20 @@
-resource "azurerm_eventhub_authorization_rule" "func" {
+resource "azurerm_eventhub_namespace_authorization_rule" "func" {
   for_each = { for each in local.func_apps_instances : each.key => each }
 
-  name           = each.value.app_name
-  namespace_name = azurerm_eventhub_namespace.eh[each.value.location].name
-
-  eventhub_name       = azurerm_eventhub.eh_business_notifications[each.value.location].name
+  name                = each.value.app_name
+  namespace_name      = azurerm_eventhub_namespace.eh[each.value.location].name
   resource_group_name = azurerm_resource_group.eh[each.value.location].name
 
   listen = true
-  send   = false
+  send   = true
   manage = false
 }
 
 resource "azurerm_key_vault_secret" "func_eh" {
   for_each = { for each in local.func_apps_instances : each.key => each }
 
-  name         = format("%s-%s", each.value.app_name, azurerm_eventhub.eh_business_notifications[each.value.location].name)
-  value        = azurerm_eventhub_authorization_rule.func[each.key].primary_connection_string
+  name         = format("%s-%s", each.value.app_name, azurerm_eventhub_namespace.eh[each.value.location].name)
+  value        = azurerm_eventhub_namespace_authorization_rule.func[each.key].primary_connection_string
   key_vault_id = azurerm_key_vault.kv[each.value.location].id
 
   depends_on = [
