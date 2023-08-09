4e110ad4bb1555ecbda8205ba242c98dda4a9e88
Fraser Molyneux <fmolyneux@microsoft.com>
2023-08-09 12:42:51 +0100

    Adding metric and event examples

--- src/func-app-sub/FraudCallDetection.cs
+++ src/func-app-sub/FraudCallDetection.cs
@@ -1,25 +1,75 @@
 using Azure.Messaging.ServiceBus;
+using Company.Abstractions.Models;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace func_app_sub
 {
     public class FraudCallDetection
     {
+        private readonly IConfiguration configuration;
+        private readonly TelemetryClient telemetryClient;
+
+        public FraudCallDetection(IConfiguration configuration, TelemetryConfiguration telemetryConfiguration)
+        {
+            this.configuration = configuration;
+            this.telemetryClient = new TelemetryClient(telemetryConfiguration);
+        }
+
         [Function("FraudCallDetection")]
         public async Task RunFraudCallDetection([EventHubTrigger("fraud-call-detection", Connection = "eventhub_connection_string")] string[] input, FunctionContext executionContext)
         {
-            var logger = executionContext.GetLogger("HttpFunction");
-            logger.LogDebug("FraudCallDetection has received a message");
+            var logger = executionContext.GetLogger("FraudCallDetection");
 
-            foreach (string messageBatch in input)
+            foreach (string message in input)
             {
-                await using (var client = new ServiceBusClient(Environment.GetEnvironmentVariable("servicebus_connection_string")))
+                // The service bus client will automatically track as a dependency to application insights
+                await using (var client = new ServiceBusClient(configuration["servicebus_connection_string"]))
                 {
+                    FraudCallDetetectionData? messageData;
+                    try
+                    {
+                        messageData = JsonConvert.DeserializeObject<FraudCallDetetectionData>(message);
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.LogError(ex, $"Exception deserializing fraud call detection payload");
+                        continue;
+                    }
+
+                    if (messageData == null)
+                    {
+                        logger.LogError($"Failed to deserialize fraud call detection payload");
+                        continue;
+                    }
+
                     var sender = client.CreateSender("fraud_call_detections");
-                    await sender.SendMessageAsync(new ServiceBusMessage(messageBatch));
+                    await sender.SendMessageAsync(new ServiceBusMessage(message));
+
+                    EventTelemetry eventTelemetry = new EventTelemetry("FraudCallDetectionInInterface");
+                    eventTelemetry.Properties.Add("InterfaceId", "ID_FraudCallDetection");
+                    eventTelemetry.Properties.Add("CallingIMSI", messageData.CallingIMSI);
+                    eventTelemetry.Properties.Add("CalledIMSI", messageData.CalledIMSI);
+                    eventTelemetry.Properties.Add("MSRN", messageData.MSRN);
+                    telemetryClient.TrackEvent(eventTelemetry);
                 };
+
+
             }
+
+            MetricTelemetry metricTelemetry = new()
+            {
+                Name = "FraudCallDetectionBatch",
+                Sum = input.Count()
+            };
+
+            metricTelemetry.Properties.Add("InterfaceId", "ID_FraudCallDetection");
+            telemetryClient.TrackMetric(metricTelemetry);
         }
     }
 }
--- src/func-app-sub/Program.cs
+++ src/func-app-sub/Program.cs
@@ -1,7 +1,23 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureAppConfiguration(configuration =>
+    {
+        var config = configuration.SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+            .AddUserSecrets<Program>();
+
+        var builtConfig = config.Build();
+    })
+    .ConfigureServices(services =>
+    {
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+    })
     .Build();
 
 host.Run();
