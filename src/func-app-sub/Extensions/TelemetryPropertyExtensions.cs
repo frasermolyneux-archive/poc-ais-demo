7142076fbb2e278b93bbce3677df9a29161c5d26
Fraser Molyneux <fmolyneux@microsoft.com>
2023-08-09 16:27:09 +0100

    Adding additional telemetry on function app

---
+++ src/func-app-sub/Extensions/TelemetryPropertyExtensions.cs
@@ -0,0 +1,73 @@
+﻿using Azure.Messaging.ServiceBus;
+using Microsoft.ApplicationInsights.DataContracts;
+
+namespace func_app_sub.Extensions
+{
+    internal static class TelemetryPropertyExtensions
+    {
+        public static ServiceBusMessage WithCustomProperties(this ServiceBusMessage message, IDictionary<string, string> additionalProperties)
+        {
+            foreach (var property in additionalProperties)
+            {
+                if (!message.ApplicationProperties.ContainsKey(property.Key))
+                {
+                    message.ApplicationProperties.Add(property.Key, property.Value);
+                }
+            }
+
+            return message;
+        }
+
+        public static EventTelemetry WithCustomProperties(this EventTelemetry telemetry, IDictionary<string, string> additionalProperties)
+        {
+            foreach (var property in additionalProperties)
+            {
+                if (!telemetry.Properties.ContainsKey(property.Key))
+                {
+                    telemetry.Properties.Add(property.Key, property.Value);
+                }
+            }
+
+            return telemetry;
+        }
+
+        public static MetricTelemetry WithCustomProperties(this MetricTelemetry telemetry, IDictionary<string, string> additionalProperties)
+        {
+            foreach (var property in additionalProperties)
+            {
+                if (!telemetry.Properties.ContainsKey(property.Key))
+                {
+                    telemetry.Properties.Add(property.Key, property.Value);
+                }
+            }
+
+            return telemetry;
+        }
+
+        public static TraceTelemetry WithCustomProperties(this TraceTelemetry telemetry, IDictionary<string, string> additionalProperties)
+        {
+            foreach (var property in additionalProperties)
+            {
+                if (!telemetry.Properties.ContainsKey(property.Key))
+                {
+                    telemetry.Properties.Add(property.Key, property.Value);
+                }
+            }
+
+            return telemetry;
+        }
+
+        public static ExceptionTelemetry WithCustomProperties(this ExceptionTelemetry telemetry, IDictionary<string, string> additionalProperties)
+        {
+            foreach (var property in additionalProperties)
+            {
+                if (!telemetry.Properties.ContainsKey(property.Key))
+                {
+                    telemetry.Properties.Add(property.Key, property.Value);
+                }
+            }
+
+            return telemetry;
+        }
+    }
+}
--- src/func-app-sub/FraudCallDetection.cs
+++ src/func-app-sub/FraudCallDetection.cs
@@ -1,11 +1,11 @@
 using Azure.Messaging.ServiceBus;
 using Company.Abstractions.Models;
+using func_app_sub.Extensions;
 using Microsoft.ApplicationInsights;
 using Microsoft.ApplicationInsights.DataContracts;
 using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.Diagnostics;
 
@@ -27,48 +27,49 @@ namespace func_app_sub
         {
             Activity.Current?.AddBaggage("InterfaceId", "ID_FraudCallDetection");
 
-            var logger = executionContext.GetLogger("FraudCallDetection");
-
-            var customDimensions = new Dictionary<string, object>()
+            var customDimensions = new Dictionary<string, string>()
             {
-                {"InterfaceId", "ID_FraudCallDetection" }
+                {"InterfaceId", "ID_FraudCallDetection"}
             };
 
-            using (logger.BeginScope(customDimensions))
+            foreach (string message in input)
             {
-                foreach (string message in input)
+                // The service bus client will automatically track as a dependency to application insights
+                await using (var client = new ServiceBusClient(configuration["servicebus_connection_string"]))
                 {
-                    // The service bus client will automatically track as a dependency to application insights
-                    await using (var client = new ServiceBusClient(configuration["servicebus_connection_string"]))
+                    FraudCallDetetectionData? messageData;
+                    try
                     {
-                        FraudCallDetetectionData? messageData;
-                        try
-                        {
-                            messageData = JsonConvert.DeserializeObject<FraudCallDetetectionData>(message);
-                        }
-                        catch (Exception ex)
-                        {
-                            logger.LogError(ex, $"Exception deserializing fraud call detection payload");
-                            continue;
-                        }
+                        messageData = JsonConvert.DeserializeObject<FraudCallDetetectionData>(message);
+                    }
+                    catch (Exception ex)
+                    {
+                        telemetryClient.TrackException(new ExceptionTelemetry(ex).WithCustomProperties(customDimensions));
+                        continue;
+                    }
 
-                        if (messageData == null)
-                        {
-                            logger.LogError($"Failed to deserialize fraud call detection payload");
-                            continue;
-                        }
+                    if (messageData == null)
+                    {
+                        telemetryClient.TrackException(new Exception("Message was null after deserialization attempt"), customDimensions);
+                        continue;
+                    }
 
-                        var sender = client.CreateSender("fraud_call_detections");
-                        await sender.SendMessageAsync(new ServiceBusMessage(message));
-
-                        EventTelemetry eventTelemetry = new("FraudCallDetectionInInterface");
-                        eventTelemetry.Properties.Add("InterfaceId", "ID_FraudCallDetection");
-                        eventTelemetry.Properties.Add("CallingIMSI", messageData.CallingIMSI);
-                        eventTelemetry.Properties.Add("CalledIMSI", messageData.CalledIMSI);
-                        eventTelemetry.Properties.Add("MSRN", messageData.MSRN);
-                        telemetryClient.TrackEvent(eventTelemetry);
+                    var messageCustomDimensions = new Dictionary<string, string>()
+                    {
+                        {"InterfaceId", "ID_FraudCallDetection"},
+                        {"CallingIMSI", messageData.CallingIMSI},
+                        {"CalledIMSI", messageData.CalledIMSI},
+                        {"MSRN", messageData.MSRN}
                     };
-                }
+
+                    telemetryClient.TrackTrace(new TraceTelemetry($"The message originated from {messageData.SwitchNum}").WithCustomProperties(messageCustomDimensions));
+
+                    var sender = client.CreateSender("fraud_call_detections");
+                    await sender.SendMessageAsync(new ServiceBusMessage(message).WithCustomProperties(messageCustomDimensions));
+
+                    EventTelemetry eventTelemetry = new EventTelemetry("FraudCallDetectionInInterface").WithCustomProperties(messageCustomDimensions);
+                    telemetryClient.TrackEvent(eventTelemetry);
+                };
 
                 MetricTelemetry metricTelemetry = new()
                 {
@@ -78,6 +79,7 @@ namespace func_app_sub
 
                 metricTelemetry.Properties.Add("InterfaceId", "ID_FraudCallDetection");
                 telemetryClient.TrackMetric(metricTelemetry);
+
             }
         }
     }
