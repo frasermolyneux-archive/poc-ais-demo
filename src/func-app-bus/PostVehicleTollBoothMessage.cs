0b7ba85a5bc40041d912f64817be3ce7200f18d6
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-11-07 22:28:55 +0000

    Something not working with this

--- src/func-app-bus/PostVehicleTollBoothMessage.cs
+++ src/func-app-bus/PostVehicleTollBoothMessage.cs
@@ -4,6 +4,7 @@ using Microsoft.ApplicationInsights.DataContracts;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
@@ -20,8 +21,31 @@ namespace Company.Functions.Bus
 
         [FunctionName("PostVehicleTollBoothMessage")]
         [return: ServiceBus("vehicle_toll_booth", Connection = "servicebus_connection_string")]
-        public string RunPostVehicleTollMessage([HttpTrigger] HttpRequest req, [FromBody] VehicleTollBoothData messageData, ILogger logger)
+        public async Task<string> RunPostVehicleTollMessage([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ILogger logger)
         {
+            string requestBody = string.Empty;
+            using (StreamReader streamReader = new StreamReader(req.Body))
+            {
+                requestBody = await streamReader.ReadToEndAsync();
+            }
+
+            VehicleTollBoothData? messageData;
+            try
+            {
+                messageData = JsonConvert.DeserializeObject<VehicleTollBoothData>(requestBody);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"Error deserializing vehicle toll booth event: {ex.Message}");
+                throw;
+            }
+
+            if (messageData is null)
+            {
+                logger.LogError($"Unable to deserialize vehicle toll booth event: {requestBody}");
+                throw new Exception("Unable to deserialize vehicle toll booth event");
+            }
+
             scopedTelemetryClient.SetAdditionalProperty("InterfaceId", "ID_VehicleTollBooth");
 
             var messageCustomDimensions = new Dictionary<string, string>()
